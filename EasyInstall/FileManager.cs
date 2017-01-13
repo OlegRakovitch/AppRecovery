f3ef03971c7455faa399e0b294add01950bfa70c
PavelSchetinin <Abilatix@gmail.com>
2017-01-13 19:19:05 +0300

    Added administrator rights. Added other application start.

---
+++ EasyInstall/FileManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyInstall
+{
+    class FileManager
+    {
+        public static void RunningApp()
+        {
+            Process run = new Process();
+            run.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            run.Start();
+        }
+    }
+}
