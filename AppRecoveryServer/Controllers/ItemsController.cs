860cbbf723b444717cfbd7367aca11eccfbf9242
Geslot <devgeslot@gmail.com>
2017-01-13 20:03:17 +0300

    Added unimplemented controllers for all endpoints

---
+++ AppRecoveryServer/Controllers/ItemsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppRecoveryServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        //GET api/items
+        //Get all items
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpGet]
+        public String Get(
+            [FromBody]String clientId,
+            [FromBody]String clientSecret)
+        {
+            throw new NotImplementedException();
+        }
+
+        //POST api/items
+        //Post new item
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpPost]
+        public String Post(
+            [FromBody]String clientId,
+            [FromBody]String clientSecret,
+            [FromBody]String caption,
+            [FromBody]String description,
+            [FromBody]int order,
+            [FromBody]String link)
+        {
+            throw new NotImplementedException();
+        }
+
+        //PATCH api/items/1
+        //Modify specified item
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpPatch("{id}")]
+        public String Patch(int id,
+            [FromBody]String clientId,
+            [FromBody]String clientSecret,
+            [FromBody]String caption,
+            [FromBody]String description,
+            [FromBody]int order,
+            [FromBody]String link)
+        {
+            throw new NotImplementedException();
+        }
+
+        //DELETE api/items/1
+        //Delete specified item
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpDelete("{id}")]
+        public String Delete(int id,
+            [FromBody]String clientId,
+            [FromBody]String clientSecret)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppRecoveryServer/Controllers/LogonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppRecoveryServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class LogonController : Controller
+    {
+        //POST api/logon
+        //SignIn
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpPost]
+        public String Post(
+            [FromBody]String clientId,
+            [FromBody]String clientSecret)
+        {
+            throw new NotImplementedException();
+        }
+
+        //PUT api/logon
+        //SignUp
+#if !DEBUG
+        [RequireHttps]
+#endif
+        [HttpPut]
+        public String Put(
+            [FromBody]String clientId,
+            [FromBody]String email,
+            [FromBody]String clientSecret)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
